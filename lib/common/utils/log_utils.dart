1bd64b55ac7dc4ffa15ce660d39db518d7f0bfe6
Huy Bảo <baohuy.phan1996@gmail.com>
2020-06-14 10:05:58 +0700

    - update logUtils

--- lib/common/utils/log_utils.dart
+++ lib/common/utils/log_utils.dart
@@ -1,5 +1,7 @@
 import 'package:logger/logger.dart';
 
+import '../../envs.dart';
+
 class LogUtils {
   static final Logger _logger = Logger(
     printer: PrettyPrinter(printTime: true),
@@ -9,12 +11,27 @@ class LogUtils {
     printer: PrettyPrinter(methodCount: 0),
   );
 
-  static void d(dynamic message, [dynamic error, StackTrace stackTrace]) =>
+  static void d(dynamic message, [dynamic error, StackTrace stackTrace]) {
+    if (appConfig?.cheat == true) {
       _loggerNoStack.d(message, error, stackTrace);
-  static void e(dynamic message, [dynamic error, StackTrace stackTrace]) =>
+    }
+  }
+
+  static void e(dynamic message, [dynamic error, StackTrace stackTrace]) {
+    if (appConfig?.cheat == true) {
       _logger.e(message, error, stackTrace);
-  static void i(dynamic message, [dynamic error, StackTrace stackTrace]) =>
+    }
+  }
+
+  static void i(dynamic message, [dynamic error, StackTrace stackTrace]) {
+    if (appConfig?.cheat == true) {
       _loggerNoStack.i(message, error, stackTrace);
-  static void w(dynamic message, [dynamic error, StackTrace stackTrace]) =>
+    }
+  }
+
+  static void w(dynamic message, [dynamic error, StackTrace stackTrace]) {
+    if (appConfig?.cheat == true) {
       _loggerNoStack.w(message, error, stackTrace);
+    }
+  }
 }
