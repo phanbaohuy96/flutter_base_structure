d896446e33051523285a4b3409cdf1fd9c9aba11
Bảo Huy <>
2021-10-25 11:00:56 +0700

    - update input container

--- lib/presentation/common_widget/input_container/input_container.dart
+++ lib/presentation/common_widget/input_container/input_container.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../../common/constants.dart';
+import '../../theme/theme_color.dart';
 
 part 'input_container.controller.dart';
 
@@ -9,12 +9,14 @@ class InputContainer extends StatelessWidget {
   final String? hint;
   final bool isPassword;
   final bool readOnly;
+  final bool enable;
   final Widget? suffixIcon;
   final TextInputType? keyboardType;
   final TextCapitalization textCapitalization;
-  final Function()? onTap;
+  final void Function()? onTap;
   final Function(String)? onTextChanged;
   final int maxLines;
+  final String? title;
 
   const InputContainer({
     Key? key,
@@ -22,15 +24,18 @@ class InputContainer extends StatelessWidget {
     this.hint,
     this.isPassword = false,
     this.readOnly = false,
+    this.enable = true,
     this.suffixIcon,
     this.keyboardType,
     this.textCapitalization = TextCapitalization.none,
     this.onTap,
     this.onTextChanged,
     this.maxLines = 1,
+    this.title,
   }) : super(key: key);
 
   bool get hasSuffixIcon => isPassword || suffixIcon != null;
+
   double get suffixIconSize => hasSuffixIcon ? 16 : 0;
 
   @override
@@ -39,51 +44,75 @@ class InputContainer extends StatelessWidget {
     return ValueListenableBuilder<InputContainerProperties>(
       valueListenable: controller ?? InputContainerController(),
       builder: (ctx, value, w) {
+        late Widget body;
+        final textField = TextField(
+          focusNode: value.focusNode,
+          readOnly: readOnly || !enable,
+          controller: value.tdController,
+          decoration: InputDecoration(
+            filled: !enable,
+            border: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.grey, width: 1),
+              borderRadius: BorderRadius.circular(6.0),
+            ),
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 8,
+              vertical: 6,
+            ),
+            hintText: hint,
+            hintStyle: themeData.textTheme.subtitle2,
+            errorText: value.validation,
+            errorStyle: themeData.textTheme.subtitle1?.copyWith(
+              color: Colors.red,
+              fontSize: value.validation?.isNotEmpty == true ? null : 1,
+            ),
+            suffixIcon: Padding(
+              padding: EdgeInsets.symmetric(horizontal: suffixIconSize),
+              child: _getSuffixIcon(),
+            ),
+            suffixIconConstraints: BoxConstraints(
+              minHeight: suffixIconSize,
+              minWidth: suffixIconSize,
+            ),
+          ),
+          keyboardType: keyboardType,
+          textCapitalization: textCapitalization,
+          style: themeData.textTheme.bodyText2,
+          obscureText: isPassword && controller?.isShowPass != true,
+          onChanged: (text) {
+            if (value.validation != null) {
+              controller?.resetValidation();
+            }
+            onTextChanged?.call(text);
+          },
+          maxLines: maxLines,
+          onTap: onTap,
+        );
+        if (title?.isNotEmpty == true) {
+          body = Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  title!,
+                  style: themeData.textTheme.subtitle2?.copyWith(
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 6),
+              textField,
+            ],
+          );
+        } else {
+          body = textField;
+        }
         return Theme(
           data: themeData.copyWith(
             primaryColor: themeData.accentColor,
             primaryColorDark: themeData.accentColor,
           ),
-          child: TextField(
-            focusNode: value.focusNode,
-            readOnly: readOnly,
-            controller: value.tdController,
-            decoration: InputDecoration(
-              border: const OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey, width: 1),
-              ),
-              contentPadding: const EdgeInsets.symmetric(
-                horizontal: 8,
-                vertical: 6,
-              ),
-              hintText: hint,
-              hintStyle: themeData.textTheme.subtitle2,
-              errorText: value.validation,
-              errorStyle: themeData.textTheme.subtitle1?.copyWith(
-                color: Colors.red,
-              ),
-              suffixIcon: Padding(
-                padding: EdgeInsets.all(suffixIconSize),
-                child: _getSuffixIcon(),
-              ),
-              suffixIconConstraints: BoxConstraints(
-                minHeight: suffixIconSize,
-                minWidth: suffixIconSize,
-              ),
-            ),
-            keyboardType: keyboardType,
-            textCapitalization: textCapitalization,
-            style: themeData.textTheme.bodyText2,
-            obscureText: isPassword && controller?.isShowPass != true,
-            onChanged: (text) {
-              if (value.validation != null) {
-                controller?.resetValidation();
-              }
-              onTextChanged?.call(text);
-            },
-            maxLines: maxLines,
-            onTap: onTap,
-          ),
+          child: body,
         );
       },
     );
@@ -101,15 +130,11 @@ class InputContainer extends StatelessWidget {
   }
 
   Widget _getPasswordIcon() {
-    var imagePath = ImageConstant.iconEyeSlash;
-    if (controller?.isShowPass == true) {
-      imagePath = ImageConstant.iconEye;
-    }
-
-    return Image.asset(
-      imagePath,
-      width: suffixIconSize,
-      height: suffixIconSize,
+    return Icon(
+      Icons.remove_red_eye,
+      size: suffixIconSize,
+      color:
+          controller?.isShowPass == true ? AppColor.primaryColor : Colors.grey,
     );
   }
 }
