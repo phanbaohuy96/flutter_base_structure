61b7f70e839cfb1c0b71fdef2e94988514b56a58
Bảo Huy <>
2025-04-17 14:38:45 +0700

    feat: support build web from core

--- plugins/fl_media/lib/src/modules/image_cropper/image_cropper_coordinator.dart
+++ plugins/fl_media/lib/src/modules/image_cropper/image_cropper_coordinator.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:fl_utils/fl_utils.dart';
 import 'package:flutter/material.dart';
 
-import 'image_cropper_screen.dart';
+import 'image_cropper.dart';
 
 extension ImageCropperCoordinator on BuildContext {
   Future<File?> cropImage({
--- plugins/fl_media/lib/src/modules/image_cropper/image_cropper_screen.dart
+++ plugins/fl_media/lib/src/modules/image_cropper/image_cropper_screen.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+import 'dart:io' as io;
 import 'dart:typed_data';
 
 import 'package:extended_image/extended_image.dart';
@@ -20,7 +20,7 @@ class ImageCropperScreen extends StatefulWidget {
     required this.imagefile,
   }) : super(key: key);
 
-  final File imagefile;
+  final io.File imagefile;
   @override
   _ImageCropperScreenState createState() => _ImageCropperScreenState();
 }
