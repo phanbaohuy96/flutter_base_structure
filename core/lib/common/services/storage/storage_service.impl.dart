4b84284e3afe03f9107a83a79d1cecdfa2d05556
Bảo Huy <>
2025-04-18 21:29:37 +0700

    feat: enhance file picker on web

--- core/lib/common/services/storage/storage_service.impl.dart
+++ core/lib/common/services/storage/storage_service.impl.dart
@@ -20,8 +20,9 @@ class StorageServiceImpl extends StorageService {
   }) async {
     final res = autoCompressImage
         ? await _compressFileIfImage(
-            file,
-            lookupMimeType(file.path),
+            await file.readAsBytes(),
+            filePath: file.path,
+            mimeType: lookupMimeType(file.path),
             option: compressImageOption,
           ).then(
             (bytes) => _storageRepository.uploadBytes(
@@ -39,6 +40,78 @@ class StorageServiceImpl extends StorageService {
   }
 
   @override
+  Future<CloudFile?> uploadBytes(
+    Uint8List bytes,
+    String name, {
+    String? filePath,
+    String? mimeType,
+    bool autoCompressImage = true,
+    CompressImageOption compressImageOption = const CompressImageOption(),
+  }) async {
+    final res = autoCompressImage
+        ? await _compressFileIfImage(
+            bytes,
+            filePath: filePath,
+            mimeType: mimeType,
+            option: compressImageOption,
+          ).then(
+            (bytes) => _storageRepository.uploadBytes(
+              [
+                dio.MultipartFile.fromBytes(
+                  bytes.$1,
+                  filename: name,
+                ),
+              ],
+            ),
+          )
+        : await _storageRepository.uploadBytes(
+            [
+              dio.MultipartFile.fromBytes(
+                bytes,
+                filename: name,
+              ),
+            ],
+          );
+
+    return res.data;
+  }
+
+  @override
+  Future<CloudFile?> uploadImageBytes(
+    Uint8List bytes,
+    String name, {
+    bool autoCompressImage = true,
+    CompressImageOption compressImageOption = const CompressImageOption(),
+  }) async {
+    final res = autoCompressImage
+        ? await _compressHelper
+            .compressWithList(
+              image: bytes,
+              option: compressImageOption,
+            )
+            .then(
+              (bytes) => _storageRepository.uploadBytes(
+                [
+                  dio.MultipartFile.fromBytes(
+                    bytes.$1,
+                    filename: name,
+                  ),
+                ],
+              ),
+            )
+        : await _storageRepository.uploadBytes(
+            [
+              dio.MultipartFile.fromBytes(
+                bytes,
+                filename: name,
+              ),
+            ],
+          );
+
+    return res.data;
+  }
+
+  @override
   String getAssetUrl(String id) {
     return [
       assetLayer,
@@ -47,12 +120,12 @@ class StorageServiceImpl extends StorageService {
   }
 
   Future<(Uint8List image, String? mimeType)> _compressFileIfImage(
-    File file,
-    String? mimeType, {
+    Uint8List bytes, {
+    String? filePath,
+    String? mimeType,
     CompressImageOption option = const CompressImageOption(),
   }) async {
-    final mimeStr = mimeType ?? lookupMimeType(file.path);
-    final bytes = await file.readAsBytes();
+    final mimeStr = mimeType ?? lookupMimeType(filePath ?? '');
     if (mimeStr?.startsWith('image/') == true) {
       try {
         return _compressHelper.compressWithList(
--- plugins/fl_media/lib/src/services/pick_file_helper/pick_file_helper.dart
+++ plugins/fl_media/lib/src/services/pick_file_helper/pick_file_helper.dart
@@ -1,8 +1,7 @@
-import 'dart:io';
-
 import 'package:file_picker/file_picker.dart' as f_picker;
 import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:mime/mime.dart';
 
 export 'package:image_picker/image_picker.dart' show CameraDevice;
 
@@ -42,18 +41,36 @@ class PickFileHelper {
     String? initialDirectory,
     FileType type = FileType.any,
     List<String>? allowedExtensions,
-    bool allowCompression = true,
+    int compressionQuality = 0,
     bool allowMultiple = false,
     bool withData = false,
     bool withReadStream = false,
     bool lockParentWindow = false,
   }) async {
+    if (kIsWeb) {
+      /// This helper class addresses a limitation with the `file_picker`
+      /// package, which currently does not support web platforms.
+      /// As a temporary workaround, the [ImagePicker] package is used for
+      /// file picking functionality on the web.
+      ///
+      /// For more details, refer to the related issue on GitHub:
+      /// https://github.com/miguelpruivo/flutter_file_picker/issues/876
+      final picker = ImagePicker();
+      final result = await picker.pickImage(
+        source: ImageSource.gallery,
+      );
+
+      return [
+        if (result != null) await FilePicked.fromXFile(result),
+      ];
+    }
+
     final result = await f_picker.FilePicker.platform.pickFiles(
       dialogTitle: dialogTitle,
       initialDirectory: initialDirectory,
       type: type.filePickerType,
       allowedExtensions: allowedExtensions,
-      allowCompression: allowCompression,
+      compressionQuality: compressionQuality,
       allowMultiple: allowMultiple,
       withData: withData,
       withReadStream: withReadStream,
