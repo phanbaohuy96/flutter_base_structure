31ab3c21265154e5a055e68008d95008332a85bd
huyphan <admin@admins-MacBook-Pro.local>
2026-05-25 17:11:16 +0700

    fix(module_generator): align simple module barrels

--- tools/module_generator/lib/res/templates/listing_module/module.dart
+++ tools/module_generator/lib/res/templates/listing_module/module.dart
@@ -3,5 +3,4 @@ import '../../../common/definitions.dart';
 const listingModule =
     '''export '${moduleNameKey}_route.dart';
 export 'bloc/${moduleNameKey}_bloc.dart';
-export '${moduleNameKey}_coordinator.dart';
 export 'views/${moduleNameKey}_screen.dart';''';
